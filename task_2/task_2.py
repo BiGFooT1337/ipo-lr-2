dd07b3c7e4398b9c10132865e7ccc3c9d761d810
Вадим <vadimgarkin2@gmail.com>
2025-09-29 02:00:12 +0300

    task_2

--- task_2/task_2.py
+++ task_2/task_2.py
@@ -1,7 +1,8 @@
 n = int(input("Введите количество школьников:"))
 k = int(input("Введите количество яблок:"))
 
-apples_students = k // n
+
+apples_students =  k // n
 remains = k % n
 
 print("Каждому школьнику достанется:", apples_students)
